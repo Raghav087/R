07d0798ad133b7b54464f7e19d34e8918bef6956
Raghav087 <raghavsingh0487@gmail.com>
2021-07-13 22:54:27 +0530

    Adding few lines.

--- Dplyr.r
+++ Dplyr.r
@@ -9,7 +9,7 @@ library(hflights)
 d1 <- as_tibble(mtcars)
 d1
 d2 <- as_tibble(hflights)
-head(d2)
+d2
 
 d1$cyl #Gives a vector O/P having values from the 'cyl' column of 'd1'.
 d1['cyl'] #Gives a tibble/dataframe having a single 'cyl' column.
@@ -24,6 +24,8 @@ filter(hflights, DepDelay >100 | Month %in% c(2,3)) #This is an OR condition i.e
 #Using the pipe (%>%) operator.
 d1 %>% select(mpg,disp,wt,am) %>% filter(wt>3,am==0) #Select the 4 columns and then filter the rows as per the criteria specified.
 d2 %>% select(UniqueCarrier,DepDelay) %>% filter(DepDelay>60)
+d2 %>% select(contains(c('Month','Time'))) #If there are multiple columns having a prefix/suffix, we can use 'contains' function instead of typing all column names.
+
 #Select the columns, then filter the rows and then arrange/sort the data in descending order of 'DepDelay' column.
 d2 %>% select(UniqueCarrier,DepDelay) %>% filter(DepDelay>60) %>% arrange(-DepDelay) %>% print(n=20) #The 'print' function helps to print more rows in terminal than the default 10 for a tibble.
 
@@ -31,12 +33,20 @@ d2 %>% select(UniqueCarrier,DepDelay) %>% filter(DepDelay>60) %>% arrange(-DepDe
 d2_new <- d2 %>% mutate(Speed=Distance/AirTime)
 d2_new
 
+#Count the no. of flights getting delayed by >30 minutes each month.
+d2 %>% group_by(Month) %>% summarise(count_delayed = sum(DepDelay>30,na.rm=TRUE))
+
+#Count the proportion of flights getting delayed by >30 minutes each month.
+d2 %>% group_by(Month) %>% summarise(prop_delayed = mean(DepDelay>30,na.rm=TRUE))
+
 #Getting a statistic based on groups.
 aggregate(TailNum~Dest, d2, length) #Gives the number of flights landing at each destination.
 d2 %>% group_by(Dest) %>% summarise(Count=length(TailNum)) #The 'dplyr' approach for the same. For grouped statistics we will always use 'group_by' together with 'summarise' function.
 
 #For specifically getting count statistic based on groups, we can choose any of the below methods.
 d2 %>% group_by(Month,DayofMonth) %>% summarise(flight_count=n()) #Gives the no. of rows for each 'by' group i.e. no. of flights each day.
+d2 %>% count(Month,DayofMonth) #Does the same job as above. Much shorter. Hence when we need only grouped counts, we can use 'count' function.
+d2 %>% count(TailNum,wt=Distance) #Gorups the data on 'Tailnum' column and calculates the sum of values of 'Distance' column for each group levels.
 d2 %>% group_by(Month,DayofMonth) %>% tally(sort=TRUE) #Does the same job as 'n()' in above example. Giving the 'sort=TRUE' option sorts the data in descending order.
 
 #Getting count of distinct rows for each group.
@@ -63,16 +73,20 @@ iris <- as_tibble(iris)
 i1 <- iris %>% sample_n(6)
 i2 <- iris %>% sample_frac(0.6)
 
-i1 <- iris %>% filter(grepl('vi',Species)) #filter rows having 'vi' string under 'Species' column
+iris %>% filter(grepl('vi',Species)) #filter rows having 'vi' string under 'Species' column
 iris %>% summarise(Mean= mean(Sepal.Length),Median=median(Sepal.Length))
+
 iris %>% summarise_at(vars(Sepal.Length,Petal.Width),list(~n(),~mean(.),~median(.)))
 iris %>% summarise_at(vars(Sepal.Length,Petal.Length),list(~n(),~mean(.,na.rm=TRUE),missing= ~sum(is.na(.))))
+
+#To perform a bunch of operations on all columns of the dataframe, we use 'summarise_all' function.
 mtcars %>% summarise_all(list(~n(),~mean(.,na.rm=TRUE),~median(.,na.rm=TRUE)))
-mtcars %>% summarise_all(list(missing=~sum(is.na(.)))) #find missing values across each column of dataset
+mtcars %>% summarise_all(list(missing=~sum(is.na(.)))) #find missing values across each column of dataset.
 mtcars['cyl'] %>% summarise_all(list(~n(),~mean(.,na.rm=TRUE),~median(.,na.rm=TRUE)))
+
 mtcars %>% arrange(desc(disp))
 mtcars %>% arrange(cyl,desc(disp))
-mtcars %>% group_by(cyl) %>% summarise_at(vars(wt,drat),list(~n(),~mean(.)))
+
 mtcars %>% group_by(cyl) %>% arrange(drat) %>% do(tail(.,1))  #gives top 1 row from each group based on value for 'drat' column
 mtcars %>% group_by(cyl) %>% arrange(desc(drat)) %>% do(head(.,2)) #gives top 2 rows from each group based on value for 'drat' column
 mtcars %>% group_by(cyl) %>% arrange(desc(drat)) %>% slice(2) #gives 2nd highest row from each group based on value for 'drat' column
